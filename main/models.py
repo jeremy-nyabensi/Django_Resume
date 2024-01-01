60032e4ab25a4d42f9ba45339271fcda028459d8
JEREMY <xkode@gmail.com>
2024-01-01 17:15:03 +0300

    first commit

--- main/models.py
+++ main/models.py
@@ -8,21 +8,22 @@ class Skill(models.Model):
     class Meta:
         verbose_name_plural = 'Skills'
         verbose_name = 'Skill'
-    
+
     name = models.CharField(max_length=20, blank=True, null=True)
     score = models.IntegerField(default=80, blank=True, null=True)
     image = models.FileField(blank=True, null=True, upload_to="skills")
     is_key_skill = models.BooleanField(default=False)
-    
+
     def __str__(self):
         return self.name
+
 
 class UserProfile(models.Model):
 
     class Meta:
         verbose_name_plural = 'User Profiles'
         verbose_name = 'User Profile'
-    
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(blank=True, null=True, upload_to="avatar")
     title = models.CharField(max_length=200, blank=True, null=True)
@@ -31,23 +32,23 @@ class UserProfile(models.Model):
     cv = models.FileField(blank=True, null=True, upload_to="cv")
 
     def __str__(self):
-        return f'{self.user.first_name} {self.user.last_name}'
+        user = self.user
+        return f'{user.get_full_name() or "Profile of User ID: "}{user.id}'
 
 
 class ContactProfile(models.Model):
-    
+
     class Meta:
         verbose_name_plural = 'Contact Profiles'
         verbose_name = 'Contact Profile'
         ordering = ["timestamp"]
     timestamp = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(verbose_name="Name",max_length=100)
+    name = models.CharField(verbose_name="Name", max_length=100)
     email = models.EmailField(verbose_name="Email")
     message = models.TextField(verbose_name="Message")
 
     def __str__(self):
         return f'{self.name}'
-
 
 
 class Testimonial(models.Model):
@@ -57,7 +58,8 @@ class Testimonial(models.Model):
         verbose_name = 'Testimonial'
         ordering = ["name"]
 
-    thumbnail = models.ImageField(blank=True, null=True, upload_to="testimonials")
+    thumbnail = models.ImageField(
+        blank=True, null=True, upload_to="testimonials")
     name = models.CharField(max_length=200, blank=True, null=True)
     role = models.CharField(max_length=200, blank=True, null=True)
     quote = models.CharField(max_length=500, blank=True, null=True)
@@ -73,7 +75,7 @@ class Media(models.Model):
         verbose_name_plural = 'Media Files'
         verbose_name = 'Media'
         ordering = ["name"]
-	
+
     image = models.ImageField(blank=True, null=True, upload_to="media")
     url = models.URLField(blank=True, null=True)
     name = models.CharField(max_length=200, blank=True, null=True)
@@ -83,8 +85,10 @@ class Media(models.Model):
         if self.url:
             self.is_image = False
         super(Media, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.name
+
 
 class Portfolio(models.Model):
 
@@ -154,4 +158,3 @@ class Certificate(models.Model):
 
     def __str__(self):
         return self.name
-
